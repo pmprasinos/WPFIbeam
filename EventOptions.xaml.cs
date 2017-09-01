deab90f9707b901ce1ac66c103f636aed38a50d8
pmprasinos <pmprasinos@gmail.com>
2017-09-01 11:32:50 -0500

    rev.9

---
+++ EventOptions.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using DockingLibrary;
+
+namespace WPFiBeam
+{
+    /// <summary>
+    /// Interaction logic for EventOptions.xaml
+    /// </summary>
+    public partial class EventOptions : System.Windows.Window
+    {
+        public EventOptions()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoad(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
